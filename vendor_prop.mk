6be48f1e9eb493c0805c18c6704b0443569d9f3c
Murat Kozan <muratkozan350@gmail.com>
2019-03-02 09:15:01 +0300

    kuntao: Add missing perf properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -123,7 +123,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     vendor.iop.enable_uxe=1 \
-    vendor.perf.iop_v3.enable=1
+    vendor.perf.iop_v3.enable=1 \
+    ro.vendor.at_library=libqti-at.so \
+    persist.vendor.qti.games.gt.prof=1 \
+    vendor.perf.gestureflingboost.enable=1 \
+    ro.vendor.scroll.preobtain.enable=1 \
+    vendor.perf.dolphin.enable=1 \
+    vendor.iop.enable_prefetch_ofr=1
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
